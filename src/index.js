0facbe6c7a1fac98836e070085381d81fc2c9d67
litmus-zhang <abdulsalamlukmon9@gmail.com>
2022-05-29 19:47:54 +0100

    near market place completed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeContract } from './utils/near';
+import "bootstrap"
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.nearInitPromise = initializeContract().then(() =>
+{
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  }
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
